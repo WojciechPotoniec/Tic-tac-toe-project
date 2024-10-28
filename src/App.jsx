5bb4aa612ded7ea340c0c5d19000063c2b6fa3ec
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-10-28 15:02:35 +0100

    created component to manage players

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <p style={{ color: 'black', textAlign: 'center' }}>Coming soon...</p>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        Game Board
+      </div>
+      LOG
+    </main>
+  );
 }
 
-export default App
+export default App;
